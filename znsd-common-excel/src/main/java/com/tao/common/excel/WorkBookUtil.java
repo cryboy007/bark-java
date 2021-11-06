cd9d5f179f9f89913181cf69ddf40b3bdaf187e2
cryboy007 <38103600+cryboy007@users.noreply.github.com>
2021-11-06 16:41:32 +0800

    Delete znsd-common-excel directory

--- znsd-common-excel/src/main/java/com/tao/common/excel/WorkBookUtil.java
+++
@@ -1,8 +0,0 @@
-package com.tao.common.excel;
-
-import org.apache.poi.ss.SpreadsheetVersion;
-
-public class WorkBookUtil {
-	public static final int MaxRowCount = SpreadsheetVersion.EXCEL2007.getMaxRows();
-	public static final String FileSuffix = "xlsx";
-}
